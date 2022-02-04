4399ee95f02dfbe3b1ddec7a2ba95ba6622d848c
CinderUA <zloupc@gmail.com>
2022-02-04 12:34:14 +0200

    dz12

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,22 +3,33 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Box myBox1 = new Box();
-        Box myBox2 = new Box();
-        Box myBox3 = new Box();
-        Box myBox4 = new Box();
-        Box myBox5 = new Box();
+        Box myBox1 = new Box(10, 20, 15, "Red", " Cardboard");
+        Box myBox2 = new Box(5, 7, 15, "Green", " Paper");
+        Box myBox3 = new Box(12, 7, 15, "Yellow", " Plastic");
+        Box myBox4 = new Box(12, 6, 18, "Pink", " Textile");
+        Box myBox5 = new Box(10, 2, 15, "Brown", " Cardboard");
 
-        myBox1.personalization(10, 20, 15);
-        myBox2.personalization(6, 3, 3);
-        myBox3.personalization(12, 7, 15);
-        myBox4.personalization(12, 6, 18);
-        myBox5.personalization(10, 2, 15);
+        myBox1.setDepth(20);
+        System.out.println(myBox1.getDepth());
+        myBox1.setWidth(10);
+        System.out.println(myBox1.getWidth());
+        myBox1.setHeight(0);
+        System.out.println(myBox1.getHeight());
+        System.out.println();
 
-        System.out.println("Объем 1й коробки: " + myBox1.count());
-        System.out.println("Объем 2й коробки: " + myBox2.count());
-        System.out.println("Объем 3й коробки: " + myBox3.count());
-        System.out.println("Объем 4й коробки: " + myBox4.count());
-        System.out.println("Объем 5й коробки: " + myBox5.count());
+        System.out.println("The volume of the 1st box: " + myBox1.count());
+        System.out.println("Color and material of the 1st box: " + myBox1.getColor() + myBox1.getMaterial());
+
+        System.out.println("The volume of the 2st box: " + myBox2.count());
+        System.out.println("Color and material of the 2st box: " + myBox2.getColor() + myBox2.getMaterial());
+
+        System.out.println("The volume of the 3st box: " + myBox3.count());
+        System.out.println("Color and material of the 3st box: " + myBox3.getColor() + myBox3.getMaterial());
+
+        System.out.println("The volume of the 4st box: " + myBox4.count());
+        System.out.println("Color and material of the 4st box: " + myBox4.getColor() + myBox4.getMaterial());
+
+        System.out.println("The volume of the 5st box: " + myBox5.count());
+        System.out.println("Color and material of the 5st box: " + myBox5.getColor() + myBox5.getMaterial());
     }
 }
--- src/com/company/Program.java
+++ src/com/company/Program.java
@@ -1,16 +1,59 @@
 package com.company;
 
 class Box {
-    double height = 10;
-    double depth = 10;
-    double width = 10;
+    private double height = 10;
+    private double depth = 10;
+    private double width = 10;
+    private String color = "Red";
+    private String material = "Cardboard";
 
-    double count() {
+    public double count() {
         return width * height * depth;
     }
-    void personalization(double w, double h, double d) {
-        width = w;
-        height = h;
-        depth = d;
+    public Box(double width, double height, double depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+    public Box(double width, double height, double depth, String color, String material) {
+        this(width, height, depth);
+        this.color = color;
+        this.material = material;
+    }
+    public double getHeight(){
+        return height;
+    }
+    public void setHeight(double height){
+        if (height > 0) {
+            this.height = height;
+        } else{
+            System.out.println("Attention! Number cannot be negative or equal to 0! So we return the previous value:");
+        }
+    }
+    public double getDepth(){
+        return depth;
+    }
+    public void setDepth(double depth){
+        if (depth > 0) {
+            this.depth = depth;
+        } else{
+            System.out.println("Attention! Number cannot be negative or equal to 0! So we return the previous value:");
+        }
+    }
+    public double getWidth(){
+        return width;
+    }
+    public void setWidth(double width){
+        if (width > 0) {
+            this.width = width;
+        } else{
+            System.out.println("Attention! Number cannot be negative or equal to 0! So we return the previous value:");
+        }
+    }
+    public String getColor(){
+        return color;
+    }
+    public String getMaterial(){
+        return material;
     }
 }
